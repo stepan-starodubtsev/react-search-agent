a1513e0fc4877932326c10bef5f3b71504e82a9f
Stepan Starodubtsev <stepan.starodubtsev@devops.ncia.nato.int>
2025-11-08 17:43:49 +0200

    simple reAct search agent done

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from langchain import hub
+from langchain.agents import AgentExecutor
+from langchain.agents.react.agent import create_react_agent
+from langchain_openai import ChatOpenAI
+from langchain_tavily import TavilySearch
+
+tools = [TavilySearch()]
+llm = ChatOpenAI(model="gpt-4")
+react_prompt = hub.pull("hwchase17/react")
+agent = create_react_agent(llm=llm, tools=tools, prompt=react_prompt)
+
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+chain = agent_executor
+
+
 def main():
-    print("Hello from react-search-agent!")
+    result = chain.invoke(
+        input={
+            "input": "search for 5 job postings for an ai engineer using Langchain in the Kyiv on Linkedin and list their details",
+        }
+    )
+    print(result)
 
 
 if __name__ == "__main__":
